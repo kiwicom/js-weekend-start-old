9b7f9d332a6e8b1c82d33115251a3707058c75e2
Petr Snobelt <petr.snobelt@kiwi.com>
2019-01-25 18:49:37 +0100

    finished next setup

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,14 @@
-export default () => <h1>Hello JS Weekend</h1>;
+import Alert from "@kiwicom/orbit-components/lib/Alert"
+import Illustration from "@kiwicom/orbit-components/lib/Illustration"
+
+export default () => (
+  <>
+    <Alert type="success">Hello, Welcome to JS WEEKEND!</Alert>
+    <Illustration
+      size="medium"
+      name="Accommodation"
+      dataTest="test"
+      spaceAfter={null}
+    />
+  </>
+)
